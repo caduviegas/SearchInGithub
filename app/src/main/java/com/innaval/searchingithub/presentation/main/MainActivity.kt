7185f5eff9a7e39adfc99d32c22d4c91ebb706d5
eduardoviegas <engenedu@hotmail.com>
2023-06-02 20:38:56 -0300

    Ajuste MainActivity, criação de xml fragments e logica de animação inicial

--- app/src/main/java/com/innaval/searchingithub/presentation/main/MainActivity.kt
+++ app/src/main/java/com/innaval/searchingithub/presentation/main/MainActivity.kt
@@ -3,10 +3,15 @@ package com.innaval.searchingithub.presentation.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.innaval.searchingithub.R
+import com.innaval.searchingithub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Thread.sleep(3000)
+        setTheme(R.style.Theme_Main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
