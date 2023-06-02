aa9b824ed502bff09c634a342e8d30d39658fd59
eduardoviegas <engenedu@hotmail.com>
2023-06-02 14:14:39 -0300

    criação de models

--- app/src/main/java/com/innaval/searchingithub/domain/model/UserInformationsModel.kt
+++ app/src/main/java/com/innaval/searchingithub/domain/model/UserInformationsModel.kt
@@ -1,4 +1,17 @@
 package com.innaval.searchingithub.domain.model
 
-class UserInformationsModel {
-}
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UserInformationsModel(
+val name: String,
+@SerializedName("watchers_count")
+val watchersCount: Int,
+@SerializedName("forks_count")
+val forksCount: Int,
+@SerializedName("stargazers_count")
+val stargazersCount: Int,
+val language: String,
+) : Parcelable
--- app/src/main/java/com/innaval/searchingithub/domain/model/UserModel.kt
+++ app/src/main/java/com/innaval/searchingithub/domain/model/UserModel.kt
@@ -1,4 +1,18 @@
 package com.innaval.searchingithub.domain.model
 
-class UserModel {
-}
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UserModel(
+    val id: Long?,
+    @SerializedName("avatar_url")
+    val avatarUrl: String?,
+    val login: String,
+    val name: String?,
+    val followers: Int?,
+    val following: Int?,
+    @SerializedName("public_repos")
+    val publicRepos: Int?,
+) : Parcelable
